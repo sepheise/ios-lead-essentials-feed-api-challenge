054a99b74da1334c6beb04f43b9a23cf6744ee82
Patricio Sepúlveda Heise <sep.heise@gmail.com>
2021-08-23 21:39:53 -0400

    Deliver invalid data error on HTTP status code 200 response if the data is an invalid JSON

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,13 +21,16 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case let .success((_, response)):
-				if response.statusCode != 200 {
+			case let .success((data, response)):
+				guard response.statusCode == 200, let _ = try? JSONDecoder().decode(Root.self, from: data) else {
 					completion(.failure(Error.invalidData))
+					return
 				}
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
 	}
+
+	private struct Root: Decodable {}
 }
