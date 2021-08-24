721cfdd99f665e6ed1a7717862a07d73326161f7
Patricio Sepúlveda Heise <sep.heise@gmail.com>
2021-08-23 20:39:04 -0400

    Deliver invalid data error on HTTP status code different than 200

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,10 +21,12 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
+			case let .success((_, response)):
+				if response.statusCode != 200 {
+					completion(.failure(Error.invalidData))
+				}
 			case .failure:
 				completion(.failure(Error.connectivity))
-			default:
-				break
 			}
 		}
 	}
